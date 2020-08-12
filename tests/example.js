9b0a388231d533868e82bf374cbfc3bd3ebfb796
Lidii <lidiiany92@gmail.com>
2020-08-12 13:41:45 -0700

    asserting elements

--- tests/example.js
+++ tests/example.js
@@ -1,6 +1,16 @@
 describe('First Steps with WebdriverIO', () => {
 	it('Load Example Website', () => {
 		browser.url('http://www.example.com');
-		browser.pause(3000);
+		//browser.pause(3000);
+		expect(browser).toHaveUrl('http://www.example.com/');
+		expect(browser).toHaveTitle('Example Domain');
+	});
+
+	it('H1 should be visible', () => {
+		//Get selector and sav it to a variable
+		const h1 = $('h1');
+
+		//Assertion
+		expect(h1).toBeVisible();
 	});
 });
